cdc4ddbdae5e01caf2e400382279d727a32326b5
BearBiscuit <742035327@qq.com>
2023-01-09 20:11:05 +0800

    init repo

---
+++ tools/run_product.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+python3 /home/dgl/workspace/launch.py \
+--workspace /home/dgl/workspace/ \
+--num_trainers 1 \
+--num_samplers 0 \
+--num_servers 1 \
+--part_config data/ogb-product.json \
+--ip_config ip_config.txt \
+"python3 train_dist.py --graph_name ogb-product --ip_config ip_config.txt --num_epochs 5 --batch_size 1000 --num_layers 3 --fan_out '10,10,10' --num_hidden 512"
