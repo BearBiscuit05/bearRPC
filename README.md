55fbb1895036f802654abf9c1e3d66270ea66205
BearBiscuit <742035327@qq.com>
2023-01-16 11:02:49 +0800

    [set] add timestamp module

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # xya_bearbiscuit
  
+ 完成RPC部分
+ 完成KVstore部分
+ 绑定到python
--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@
 apt-get update
 echo y | apt install cmake
 echo y | apt install git
+# git clone https://github.com/guangqianpeng/jrpc.git
